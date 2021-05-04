278756468374420dcc37f4d194dd263343a007f1
mmal73 <luisito250899@gmail.com>
2021-05-04 04:49:06 -0500

    feat: calendar implemented

---
+++ src/components/calendar/CalendarEvent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function CalendarEvent({event}) {
+    const {title, user} = event;
+    return (
+        <div>
+            <strong>{title}</strong>
+            <span>-{user.name}</span>
+        </div>
+    )
+}
--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/es-mx';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { messages } from '../../helpers/calendarMessages';
+import CalendarEvent from './CalendarEvent';
+import './CalendarScreen.css';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+moment.locale('es');
+const localizer = momentLocalizer(moment)
+const events = [{
+    title: "Primer Evento",
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+    notes: 'Notas primer evento',
+    user: {
+        _id: 1,
+        name: 'Luis'
+    }
+}];
 
 export default function CalendarScreen() {
+
+    const [currentView, setCurrentView] = useState(localStorage.getItem('currentView') || 'week');
+
+    const onDoubleClick = (e) => {
+        console.log(e)
+    }
+    const onSelect = (e) => {
+        console.log(e)
+        
+    }
+    const onViewEvent = (e) => {
+        setCurrentView(e);
+        localStorage.setItem('currentView', e);
+    }
+    const eventStyle = ( event,start,end,isSelected ) => {
+        return {
+            className: 'event-calendar'
+        }
+    };
+    const components = {
+        event: CalendarEvent, // used by each view (Month, Day, Week)
+    }
     return (
-        <div>
-            CalendarScreen
+        <div className="main-calendar">
+            <BigCalendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyle}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelect}
+                onView={onViewEvent}
+                view={currentView}
+                components={components}
+            />
         </div>
     )
 }
