a6c2bcd8c523eb47ce94fe1b67d69a1645d0bf7a
mmal73 <luisito250899@gmail.com>
2021-05-07 17:52:51 -0500

    feat: validation errors in login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,17 +1,33 @@
 import { fetchWithoutToken } from "../helpers/myFetch";
 import { types } from "../types/types";
+import Swal from "sweetalert2";
 
 export const authLoginStart = ( email, password ) => {
     return async( dispatch ) => {
         const resp = await fetchWithoutToken('auth/login', { email, password }, 'POST');
         const data = await resp.json();
         if( data.status ){
-            
+
             const { token, _id, name } = data;
             localStorage.setItem( 'token', token );
             localStorage.setItem( 'token-init-date', new Date().getTime() );
 
             dispatch( login({ _id, name }) )
+        }else{
+            let errors = data.message ;
+            if( !errors ){
+                errors = '';
+                Object.entries( data.errors ).map( error => {
+                    const _error = error[1]?.msg;
+                    errors += _error;
+                    errors += `<br>`;
+                });
+            }
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                html: errors
+            });
         }
 
     }
