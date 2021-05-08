e9d9592c8c55cb382cca5a9f56fbaacb3579e5d3
mmal73 <luisito250899@gmail.com>
2021-05-07 22:53:35 -0500

    refactoring: error messages in auth actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,38 +2,50 @@ import { fetchWithoutToken } from "../helpers/myFetch";
 import { types } from "../types/types";
 import Swal from "sweetalert2";
 
-export const authLoginStart = ( email, password ) => {
-    return async( dispatch ) => {
-        const resp = await fetchWithoutToken('auth/login', { email, password }, 'POST');
-        const data = await resp.json();
-        if( data.status ){
+const saveToken = ( token ) => {
+    localStorage.setItem( 'token', token );
+    localStorage.setItem( 'token-init-date', new Date().getTime() );
+}
 
-            const { token, _id, name } = data;
-            localStorage.setItem( 'token', token );
-            localStorage.setItem( 'token-init-date', new Date().getTime() );
-
-            dispatch( login({ _id, name }) )
-        }else{
-            let errors = data.message ;
-            if( !errors ){
-                errors = '';
-                Object.entries( data.errors ).map( error => {
-                    const _error = error[1]?.msg;
-                    errors += _error;
-                    errors += `<br>`;
-                });
+const showErrors = (message, errors) => {
+    if( !message ){
+        message = '';
+        for (const field in errors) {
+            if (errors.hasOwnProperty(field)) {
+                const _error = errors[field].msg;
+                message += _error;
+                message += `<br>`;
             }
-            return Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                html: errors
-            });
         }
-
     }
+    return Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        html: message
+    });
 }
 
 const login = ( user ) => ({
     type: types.authLogin,
     payload: user
 });
+
+const authLoginStart = ( email, password ) => {
+    return async( dispatch ) => {
+        const resp = await fetchWithoutToken('auth/login', { email, password }, 'POST');
+        const data = await resp.json();
+        if( data.status ){
+            const { token, _id, name } = data;
+            saveToken(token);
+            dispatch( login({ _id, name }) )
+        }else{
+            showErrors( data.message, data.errors );
+        }
+
+    }
+}
+
+
+export{
+    authLoginStart,
+}
